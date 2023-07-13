4354dc399dc4cade0e647d0c95491e5fcee87d99
wint-thiri-swe <62372029+wint-thiri-swe@users.noreply.github.com>
2023-07-13 12:15:14 +0800

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -59,11 +59,42 @@ self.addEventListener('activate', event => {
     console.log('SW : activated');
 })
 
-self.addEventListener('fetch', event => {
-    console.log('fetching');
+const cacheFirst = async ({ request, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  
+    // Next try to get the resource from the network
+    try {
+      const responseFromNetwork = await fetch(request);
+      // response may be used only once
+      // we need to save clone to put one copy in cache
+      // and serve second one
+      putInCache(request, responseFromNetwork.clone());
+      return responseFromNetwork;
+    } catch (error) {
+      const fallbackResponse = await caches.match(fallbackUrl);
+      if (fallbackResponse) {
+        return fallbackResponse;
+      }
+      // when even the fallback response is not available,
+      // there is nothing we can do, but we must always
+      // return a Response object
+      return new Response("Network error happened", {
+        status: 408,
+        headers: { "Content-Type": "text/html" },
+      });
+    }
+  };
+  
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response;
-        })
+      cacheFirst({
+        request: event.request,
+        fallbackUrl: "./index.html",
+      }),
     );
-});
+  });
